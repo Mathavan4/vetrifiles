425885c025633acc2d84f1e154f9d29a81fad0d5
Mathavan4 <mathavan1100@gmail.com>
2025-11-07 14:59:56 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 ROOT_URLCONF = 'myproject.urls'
 
 TEMPLATES = [
@@ -137,6 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
